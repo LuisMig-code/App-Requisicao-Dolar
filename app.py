ee45456b297729ecd427c17e4406c0d5670c6c21
Andre0n <andreon098@gmail.com>
2024-11-25 09:28:03 -0300

    Reformat `app.py` for improved readability

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ dados_timestamp = None
 dados_create_date = None
 dados_cotacao = None
 
+
 def realizaReqCambio():
     # Faz uma requisição para a API
     response = requests.get(f'https://economia.awesomeapi.com.br/last/USD')
     if response.status_code == 200:
         dados = response.json()
-        dados = dados['USDBRL'] 
-        
+        dados = dados['USDBRL']
+
         dados_alta = dados['high']
         dados_baixa = dados['low']
         dados_var = dados['varBid']
@@ -25,17 +26,41 @@ def realizaReqCambio():
         dados_timestamp = dados['timestamp']
         dados_create_date = dados['create_date']
 
-        return dados_alta,dados_baixa,dados_var,dados_timestamp,dados_create_date,dados_cotacao
-        
+        return (
+            dados_alta,
+            dados_baixa,
+            dados_var,
+            dados_timestamp,
+            dados_create_date,
+            dados_cotacao,
+        )
 
     else:
-        print("Erro ao fazer a requisição:", response.status_code)
+        print('Erro ao fazer a requisição:', response.status_code)
 
-dados_alta,dados_baixa,dados_var,dados_timestamp,dados_create_date,dados_cotacao = realizaReqCambio()
+
+(
+    dados_alta,
+    dados_baixa,
+    dados_var,
+    dados_timestamp,
+    dados_create_date,
+    dados_cotacao,
+) = realizaReqCambio()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html' , alta = dados_alta , baixa = dados_baixa , cotacao = dados_cotacao , var = dados_var , timestamp = dados_timestamp , created_date = dados_create_date)
+    return render_template(
+        'index.html',
+        alta=dados_alta,
+        baixa=dados_baixa,
+        cotacao=dados_cotacao,
+        var=dados_var,
+        timestamp=dados_timestamp,
+        created_date=dados_create_date,
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
